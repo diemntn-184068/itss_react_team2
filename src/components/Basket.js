d1da9707e955e4fcc785b45b30e978c1b8bbe15f
ngoc-diem <ngocdiem5102000@gmail.com>
2022-06-07 10:09:26 +0700

    add style

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -13,7 +13,7 @@ export default function Basket(props) {
         {cartItems.length === 0 && <div>Cart is empty</div>}
         {cartItems.map((item) => (
           <div key={item.id} className="row">
-            <div className="col-2">{item.name}</div>
+            <div className="item-name col-2">{item.name}</div>
             <div className="col-2">
               <button onClick={() => onRemove(item)} className="remove">
                 -
@@ -32,30 +32,33 @@ export default function Basket(props) {
         {cartItems.length !== 0 && (
           <>
             <hr></hr>
-            <div className="row">
-              <div className="col-2">Items Price</div>
-              <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
-            </div>
-            <div className="row">
-              <div className="col-2">Tax Price</div>
-              <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
-            </div>
-            <div className="row">
-              <div className="col-2">Shipping Price</div>
-              <div className="col-1 text-right">
-                ${shippingPrice.toFixed(2)}
-              </div>
-            </div>
+            <div className="total">
 
-            <div className="row">
-              <div className="col-2">
-                <strong>Total Price</strong>
+              <div className="row">
+                <div className="col-2">Items Price</div>
+                <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
               </div>
-              <div className="col-1 text-right">
-                <strong>${totalPrice.toFixed(2)}</strong>
+              <div className="row">
+                <div className="col-2">Tax Price</div>
+                <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
               </div>
+              <div className="row">
+                <div className="col-2">Shipping Price</div>
+                <div className="col-1 text-right">
+                  ${shippingPrice.toFixed(2)}
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-2">
+                  <strong>Total Price</strong>
+                </div>
+                <div className="col-1 text-right">
+                  <strong>${totalPrice.toFixed(2)}</strong>
+                </div>
+              </div>
+              <hr />
             </div>
-            <hr />
             <div className="row">
               <button onClick={() => alert('Implement Checkout!')}>
                 Checkout
